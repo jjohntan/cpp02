aa78d3a9fdeae05a0494a01b6fb061507b73469c
Jek John Tan <jetan@u82z10s06.42kl.edu.my>
2025-03-27 18:50:20 +0800

    update Fixed class

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 17:50:43 by jetan             #+#    #+#             */
-/*   Updated: 2025/03/27 18:03:52 by jetan            ###   ########.fr       */
+/*   Updated: 2025/03/27 18:49:54 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@
 class Fixed
 {
 	private:
-		
+		int rawBits;
+		static const int fractionalBits;
 	public:
 		Fixed();
 		~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
 };
-
 
 
 #endif

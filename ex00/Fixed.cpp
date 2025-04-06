66827fde84fb5db3bd67d8c67a2644524082c359
Jek John Tan <jetan@u91z04s02.42kl.edu.my>
2025-04-06 19:22:26 +0800

    add output message for all public members

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 17:50:46 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/06 19:12:58 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/06 19:20:24 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 Fixed::Fixed(): rawValue(0)
 {
-	std::cout << "default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &other)//Copy constructor
 {
-	std::cout << "copy constructor called" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 Fixed &Fixed::operator=(const Fixed &other)//Copy assignment operator
 {
-	std::cout << "copy assignment operator called" << std::endl;
+	std::cout << "Copy assignment operator called" << std::endl;
 	
 	if (this != &other)
 		this->rawValue = other.getRawBits();
@@ -35,13 +35,15 @@ Fixed &Fixed::operator=(const Fixed &other)//Copy assignment operator
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor" << std::endl;
+	std::cout << "Destructor called" << std::endl;
 }
+
 /**
  * @brief getter function
  */
 int Fixed::getRawBits( void ) const
 {
+	std::cout << "getRawBits member function called" << std::endl;
 	return this->rawValue;
 }
 

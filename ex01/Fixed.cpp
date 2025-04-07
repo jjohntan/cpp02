20f87e1a749a684f1d69a20a5bf2fff82bbbb0d0
Jek John Tan <jetan@u91z02s02.42kl.edu.my>
2025-04-07 15:10:07 +0800

    implement copy constructor

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 19:26:12 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/07 14:55:31 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/07 15:09:38 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ Fixed::Fixed(const float )
 	std::cout << "Float constructor called" << std::endl;
 }
 
+Fixed::Fixed(const Fixed &other)//Copy constructor
+{
+	std::cout << "Copy constructor called" << std::endl;
+	
+	*this = other;
+}
 
 Fixed::~Fixed()
 {

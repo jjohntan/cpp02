15b50c1af5c219fe7f7d1a11ead28621767198ae
Jek John Tan <jetan@u91z02s02.42kl.edu.my>
2025-04-07 14:57:22 +0800

    implement constructor takes constant integer and constructor takes constanat floating-point number

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 19:26:12 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/07 14:46:28 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/07 14:55:31 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,17 @@ Fixed::Fixed()
 {
 	std::cout << "Default constructor called" << std::endl;
 }
+
+Fixed::Fixed(const int )
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float )
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
 
 Fixed::~Fixed()
 {

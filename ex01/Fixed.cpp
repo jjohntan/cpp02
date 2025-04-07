c2df934042ce7ae73490364698e5b176c75b5ccc
Jek John Tan <jetan@u91z02s02.42kl.edu.my>
2025-04-07 15:26:11 +0800

    implement copy assigment operator

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 19:26:12 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/07 15:09:38 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/07 15:25:46 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,26 @@ Fixed::Fixed(const float )
 	std::cout << "Float constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &other)//Copy constructor
+/**
+ * @brief Copy constructor
+ */
+Fixed::Fixed(const Fixed &other)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	
 	*this = other;
+}
+
+/**
+ * @brief Copy assignment operator
+ */
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	
+	if (this != &other)
+		this->rawValue = other.getRawBits();
+	return *this;
 }
 
 Fixed::~Fixed()

33bd0788df65b722fe0bd99c7c15d9b1b1c7d005
Jek John Tan <jetan@u91z02s02.42kl.edu.my>
2025-04-07 14:47:10 +0800

    add default constructor and add default destructor

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,16 +6,19 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 19:26:12 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/07 14:22:35 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/07 14:46:28 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <iostream>
 
-Fixed::Fixed(/* args */)
+Fixed::Fixed()
 {
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor called" << std::endl;
 }

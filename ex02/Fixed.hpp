226d6c06fa89ed7b8ab2e6bd0c8fd5b38ff825c8
Jek John Tan <jetan@u90z03s03.42kl.edu.my>
2025-04-08 20:18:49 +0800

    add class

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 16:01:45 by jetan             #+#    #+#             */
+/*   Updated: 2025/04/08 20:08:14 by jetan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	private:
+		int rawValue;
+		static const int fractionalBits = 8;
+	public:
+		Fixed();//Default constructor
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed &other);//Copy constructor
+		Fixed &operator=(const Fixed &other);//Copy assignment operator
+		~Fixed();//Destructor
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		float toFloat( void ) const;
+		int toInt( void ) const;
+		//comparison operators
+		bool operator>(const Fixed &other) const;
+		bool operator<(const Fixed &other) const;
+		bool operator>=(const Fixed &other) const;
+		bool operator<=(const Fixed &other) const;
+		bool operator==(const Fixed &other) const;
+		bool operator!=(const Fixed &other) const;
+};
+
+#endif

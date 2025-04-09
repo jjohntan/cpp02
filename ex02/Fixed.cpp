ed22780726719287ebf85d0729c16307624b49cb
Jek John Tan <jetan@u90z03s03.42kl.edu.my>
2025-04-09 15:15:24 +0800

    implement default constructor, int/float constructor, copy constructor, copy assigment operator, and destructor

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,16 +6,57 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 16:01:48 by jetan             #+#    #+#             */
-/*   Updated: 2025/04/08 16:18:14 by jetan            ###   ########.fr       */
+/*   Updated: 2025/04/09 15:13:49 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed(/* args */)
+
+/**
+ * @brief Default constructor
+ */
+Fixed::Fixed()
 {
 }
 
+/**
+ * @brief integer constructor
+ */
+Fixed::Fixed(const int value)
+{
+	this->rawValue = value << fractionalBits;
+}
+
+/**
+ * @brief float constructor
+ */
+Fixed::Fixed(const float value)
+{
+	this->rawValue = roundf(value * (1 << fractionalBits));
+}
+
+/**
+ * @brief Copy constructor
+ */
+Fixed::Fixed(const Fixed &other)
+{
+	*this = other;
+}
+
+/**
+ * @brief Copy assignment operator
+ */
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	if (this != &other)
+		this->rawValue = other.getRawBits();
+	return *this;
+}
+
+/**
+ * @brief Destructor
+ */
 Fixed::~Fixed()
 {
 }
